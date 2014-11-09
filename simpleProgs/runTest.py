bdd457fa8b84a9775b5ac0805acf010a84b8eafd
Robert Wespetal <rwespetal@wisc.edu>
2014-11-09 17:43:26 -0600

    The shell script doesn't work, dyninst needs the actual executable. We can fix this another way, but for tonight I just modified runTest.py to check the archive.

--- simpleProgs/runTest.py
+++ simpleProgs/runTest.py
@@ -31,6 +31,7 @@ execName   = CmdLineFind("-e","scheduleInst")
 joblabel     = CmdLineFind("-l","run")
 num        = int(CmdLineFind("-n",1000))
 timeOut    = int(CmdLineFind("-timeout",120))
+file	   = CmdLineFind("-f", "") 
 frame = 1
 
 
@@ -67,23 +68,44 @@ while frame <= num:
             sumFile.write("Execution results for "+command+"\n")
 	print(command)
 	stdouterr = open("stderrout.txt","w")
+	check_archive = False
 	try:
 		subprocess.check_call(command,shell=True,timeout=timeOut,stdout=stdouterr,stderr=stdouterr)
-		stdouterr.close()
+		check_archive = True
 	except subprocess.CalledProcessError:
 		stdouterr.close()
 		print("rand seed "+str(frame)+" caused an error \n")
 		sumFile.write("rand seed "+str(frame)+" caused an error \n")
 		os.system("mv /tmp/threadLog.txt "+resDir+st+"/threadLog."+str(frame)+".txt")
-		os.system("mv stderrout.txt "+resDir+st+"/stderrout."+str(frame)+".txt")
 	except subprocess.TimeoutExpired:
 		stdouterr.close()
 		os.system("killall -9 scheduleInst "+sys.argv[extras+1].split("/")[-1])
 		print("rand seed "+str(frame)+" timed out \n")
-		sumFile.write("rand seed "+str(frame)+" caused an error \n")
+		sumFile.write("rand seed "+str(frame)+"  timed out \n")
 		os.system("mv /tmp/threadLog.txt "+resDir+st+"/threadLog."+str(frame)+".txt")
 		os.system("mv stderrout.txt "+resDir+st+"/stderrout."+str(frame)+".txt")
+	if check_archive:
+		try: 
+			check_command = "pbzip2 -tv " + file 
+			sumFile.write("Running check with command " + check_command + "\n")
+			subprocess.check_call(check_command,shell=True,timeout=timeOut,stdout=stdouterr,stderr=stdouterr)
+			os.system("mv stderrout.txt "+resDir+st+"/out."+str(frame)+".txt")
+			stdouterr.close()
+		except subprocess.CalledProcessError:
+			stdouterr.close()
+			print("archive test failed \n")
+			sumFile.write("rand seed "+str(frame)+" compression failure \n")
+			os.system("mv /tmp/threadLog.txt "+resDir+st+"/threadLog."+str(frame)+".txt")
+			os.system("mv stderrout.txt "+resDir+st+"/stderrout."+str(frame)+".txt")
+		except subprocess.TimeoutExpired:
+			stdouterr.close()
+			os.system("killall -9 scheduleInst "+sys.argv[extras+1].split("/")[-1])
+			print("rand seed "+str(frame)+" archive test timed out \n")
+			sumFile.write("rand seed "+str(frame)+" archive test timed out \n")
+			os.system("mv /tmp/threadLog.txt "+resDir+st+"/threadLog."+str(frame)+".txt")
+			os.system("mv stderrout.txt "+resDir+st+"/stderrout."+str(frame)+".txt")
 	print("DONE WITH " +str(frame))
+	sumFile.flush()
 	frame += 1
 
 sumFile.close()
