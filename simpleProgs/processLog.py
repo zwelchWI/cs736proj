eb011bf947484d120a2ef7c63b58c7b9cb88d51a
Zach Welch <zach@vlab-17.cs.wisc.edu>
2014-11-26 10:58:55 -0600

    I think this fixes it

--- simpleProgs/processLog.py
+++ simpleProgs/processLog.py
@@ -19,19 +19,29 @@ for line in lines:
     if p[0] == 'about':
         continue
  
-    threadId = p[1].split(':')[1]
 
 
-    if threadId not in stack.keys():
-        stack[threadId] = [[],[]]
 
     if p[0] == "[TRACE_ENTRY-":
+        threadId = p[1].split(':')[1]
+
+        if threadId not in stack.keys():
+            stack[threadId] = [[],[]]
+
+
         stack[threadId][0].append(p[3])
-    else:
+    elif p[0] == "[TRACE_EXIT-":
+        threadId = p[1].split(':')[1]
+
+        if threadId not in stack.keys():
+            stack[threadId] = [[],[]]
+
         stack[threadId][0] = stack[threadId][0][:-1]
         if len(stack[threadId][1]) == 3:
             stack[threadId][1] = stack[threadId][1][1:]
         stack[threadId][1].append(p[3])
+    else:
+        pass
 
 for key,val in stack.iteritems():
     print key+ " "+str(val)
